909c89e02d59c16b3dda57e226b6ec1cfd6ded57
anuda <anuda@live.com>
2018-06-26 13:48:49 +0530

    Initial Commit

---
+++ MathApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathApp
+//
+//  Created by Anuda Weerasinghe on 6/26/18.
+//  Copyright © 2018 Anuda Weerasinghe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
